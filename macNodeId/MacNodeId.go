c810991f9b1b878ce5446c3b84c83497d5d13ca5
unknown <narasimha.gm@gmail.com>
2015-09-23 18:31:02 +0000

    Flake source commit

---
+++ macNodeId/MacNodeId.go
@@ -0,0 +1,40 @@
+package macNodeId
+
+import (
+	"net"
+	"bytes"
+	"crypto/sha1"
+	"errors"
+)
+
+// node id length is 6 bytes
+const nodeIdLength = 6
+
+type MacNodeId struct {
+}
+
+func (n *MacNodeId) Id() ([]byte,error) {
+
+	macNodeId := make([]byte,nodeIdLength)
+	err := assignMacNodeId(&macNodeId)
+	
+	return macNodeId,err 
+}
+
+
+func assignMacNodeId(ptrId *[]byte) error {
+	inets,err := net.Interfaces()
+
+	if err != nil {
+		return errors.New("Unable to obtain mac address: "+err.Error())
+	}
+        macs := make([][]byte,len(inets))
+	for i:=0;i<len(inets);i++ {
+		macs[i] = inets[i].HardwareAddr
+	}
+	m := bytes.Join(macs,nil)
+	s :=  sha1.Sum(m)
+	copy((*ptrId),s[0:nodeIdLength])
+	return nil
+}
+
