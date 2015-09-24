a26e3d42571c75f6e8aa87b2bdeca8cda126cf1d
nmjmdr <narasimha.gm@gmail.com>
2015-09-24 23:31:23 +0530

    Update MacNodeId.go
    Used gofmt

--- macNodeId/MacNodeId.go
+++ macNodeId/MacNodeId.go
@@ -1,10 +1,10 @@
 package macNodeId
 
 import (
-	"net"
 	"bytes"
 	"crypto/sha1"
 	"errors"
+	"net"
 )
 
 // node id length is 6 bytes
@@ -13,28 +13,26 @@ const nodeIdLength = 6
 type MacNodeId struct {
 }
 
-func (n *MacNodeId) Id() ([]byte,error) {
+func (n *MacNodeId) Id() ([]byte, error) {
 
-	macNodeId := make([]byte,nodeIdLength)
+	macNodeId := make([]byte, nodeIdLength)
 	err := assignMacNodeId(&macNodeId)
-	
-	return macNodeId,err 
-}
 
+	return macNodeId, err
+}
 
 func assignMacNodeId(ptrId *[]byte) error {
-	inets,err := net.Interfaces()
+	inets, err := net.Interfaces()
 
 	if err != nil {
-		return errors.New("Unable to obtain mac address: "+err.Error())
+		return errors.New("Unable to obtain mac address: " + err.Error())
 	}
-        macs := make([][]byte,len(inets))
-	for i:=0;i<len(inets);i++ {
+	macs := make([][]byte, len(inets))
+	for i := 0; i < len(inets); i++ {
 		macs[i] = inets[i].HardwareAddr
 	}
-	m := bytes.Join(macs,nil)
-	s :=  sha1.Sum(m)
-	copy((*ptrId),s[0:nodeIdLength])
+	m := bytes.Join(macs, nil)
+	s := sha1.Sum(m)
+	copy((*ptrId), s[0:nodeIdLength])
 	return nil
 }
-
